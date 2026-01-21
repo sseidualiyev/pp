3a06f60fde483b5f3ade6fa57961c7984eb682c6
sseidualiev <seidualiev01@gmail.com>
2026-01-21 21:38:48 +0500

    multiword variable names

--- practice1/python-basics/variables.py
+++ practice1/python-basics/variables.py
@@ -35,3 +35,13 @@ myvar2 = "John"
 # my-var = "John"
 # my var = "John"
 # unallowed variable names
+
+# Camel Case
+# Each word, except the first, starts with a capital letter:
+myVariableName = "John"
+# Pascal Case
+# Each word starts with a capital letter:
+MyVariableName = "John"
+# Snake Case
+# Each word is separated by an underscore character:
+my_variable_name = "John"
