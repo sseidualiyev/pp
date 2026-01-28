36506d2fd4773ba3379401e63f109b01190df6cf
sseidualiev <seidualiev01@gmail.com>
2026-01-28 10:57:27 +0500

    evaluating boolean values from other data types

--- practice2/boolean/boolean_intro.py
+++ practice2/boolean/boolean_intro.py
@@ -9,3 +9,14 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15))
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
