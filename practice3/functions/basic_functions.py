875e2d823f6080bc7ced5d50cc93853896678ec7
sseidualiev <seidualiev01@gmail.com>
2026-02-04 11:40:56 +0500

    repetitive codes

--- practice3/functions/basic_functions.py
+++ practice3/functions/basic_functions.py
@@ -9,3 +9,15 @@ def my_function():
 my_function()
 my_function()
 my_function()
+
+temp1 = 77
+celsius1 = (temp1 - 32) * 5 / 9
+print(celsius1)
+
+temp2 = 95
+celsius2 = (temp2 - 32) * 5 / 9
+print(celsius2)
+
+temp3 = 50
+celsius3 = (temp3 - 32) * 5 / 9
+print(celsius3)
