1e86cf3e44cb812480b1e43a62c7438be002c42b
sseidualiev <seidualiev01@gmail.com>
2026-02-04 11:41:25 +0500

    returning values

--- practice3/functions/basic_functions.py
+++ practice3/functions/basic_functions.py
@@ -28,3 +28,9 @@ def fahrenheit_to_celsius(fahrenheit):
 print(fahrenheit_to_celsius(77))
 print(fahrenheit_to_celsius(95))
 print(fahrenheit_to_celsius(50))
+
+def get_greeting():
+  return "Hello from a function"
+
+message = get_greeting()
+print(message)
