a0799bea9b9f24098c80f8936690b2f837b7235d
sseidualiev <seidualiev01@gmail.com>
2026-02-04 11:45:41 +0500

    mix of positional and keyword arguments

--- practice3/functions/function_arguments.py
+++ practice3/functions/function_arguments.py
@@ -34,3 +34,8 @@ def my_function(animal, name):
   print("My", animal + "'s name is", name)
 
 my_function("Buddy", "dog")
+
+def my_function(animal, name, age):
+  print("I have a", age, "year old", animal, "named", name)
+
+my_function("dog", name = "Buddy", age = 5)
