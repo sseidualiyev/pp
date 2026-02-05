2f22999b626f73ab5a2526c12688ae491d5e481d
sseidualiev <seidualiev01@gmail.com>
2026-02-05 20:43:52 +0500

    combining args and kwargs

--- practice3/functions/args_kwargs.py
+++ practice3/functions/args_kwargs.py
@@ -58,3 +58,10 @@ def my_function(username, **details):
     print(" ", key + ":", value)
 
 my_function("emil123", age = 25, city = "Oslo", hobby = "coding")
+
+def my_function(title, *args, **kwargs):
+  print("Title:", title)
+  print("Positional arguments:", args)
+  print("Keyword arguments:", kwargs)
+
+my_function("User Info", "Emil", "Tobias", age = 25, city = "Oslo")
