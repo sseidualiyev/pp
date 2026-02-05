126166355667ca2a4a9f7ca2321222b5d76d8889
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:28:24 +0500

    .

--- practice3/inheritance/inheritance_basics.py
+++ practice3/inheritance/inheritance_basics.py
@@ -7,3 +7,7 @@ x.printname()
 class Student(Person):
   def __init__(self, fname, lname):
     #add properties etc.
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
