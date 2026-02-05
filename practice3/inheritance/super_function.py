c95adde6e2c8d647f48b707cceb5dd9111bb3903
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:29:54 +0500

    add property

--- practice3/inheritance/super_function.py
+++ practice3/inheritance/super_function.py
@@ -6,3 +6,10 @@ class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname)
     self.graduationyear = 2019
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
