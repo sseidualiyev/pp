7e68b4a4e7681e0d8046340cf8831ea7ef3f5d0e
sseidualiev <seidualiev01@gmail.com>
2026-02-05 20:29:45 +0500

    combining positional and keyword arguments

--- practice3/functions/function_arguments.py
+++ practice3/functions/function_arguments.py
@@ -78,4 +78,9 @@ def my_function(*, name):
 
 my_function(name = "Emil")
 
+def my_function(a, b, /, *, c, d):
+  return a + b + c + d
+
+result = my_function(5, 10, c = 15, d = 20)
+print(result)
 
