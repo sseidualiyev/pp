d5887e3e8bd98f73af1b9b1f4bdb4882f1bb3652
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:23:54 +0500

    methods that modify the property values

--- practice3/classes/class_methods.py
+++ practice3/classes/class_methods.py
@@ -29,3 +29,17 @@ class Person:
 
 p1 = Person("Tobias", 28)
 print(p1.get_info())
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def celebrate_birthday(self):
+    self.age += 1
+    print(f"Happy birthday! You are now {self.age}")
+
+p1 = Person("Linus", 25)
+p1.celebrate_birthday()
+p1.celebrate_birthday()
