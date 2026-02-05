da335529bf966249be1190233c9840d39ed51f86
sseidualiev <seidualiev01@gmail.com>
2026-02-05 20:45:01 +0500

    using kwargs to unpack a dictionary

--- practice3/functions/args_kwargs.py
+++ practice3/functions/args_kwargs.py
@@ -72,3 +72,9 @@ def my_function(a, b, c):
 numbers = [1, 2, 3]
 result = my_function(*numbers) # Same as: my_function(1, 2, 3)
 print(result)
+
+def my_function(fname, lname):
+  print("Hello", fname, lname)
+
+person = {"fname": "Emil", "lname": "Refsnes"}
+my_function(**person) # Same as: my_function(fname="Emil", lname="Refsnes")
