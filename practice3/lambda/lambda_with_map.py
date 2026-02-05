b6775397998c99c2f52741ec0fc1acc0eafe74e7
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:03:37 +0500

    use for adding lists

--- practice3/lambda/lambda_with_map.py
+++ practice3/lambda/lambda_with_map.py
@@ -12,3 +12,10 @@ celsius = [0, 10, 20, 30]
 
 fahrenheit = list(map(lambda c: c * 9/5 + 32, celsius))
 print(fahrenheit)
+
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+sum_list = list(map(lambda x, y: x + y, a, b))
+print(sum_list)
