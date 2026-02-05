6a50bd453d96ca22c4bb35a4de9dd0ef96fce8dd
sseidualiev <seidualiev01@gmail.com>
2026-02-05 20:56:46 +0500

    using lambda with functions

--- practice3/lambda/lambda_basics.py
+++ practice3/lambda/lambda_basics.py
@@ -6,3 +6,13 @@ print(x(5, 6))
 
 x = lambda a, b, c : a + b + c
 print(x(5, 6, 2))
+
+def myfunc(n):
+  return lambda a : a * n
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
