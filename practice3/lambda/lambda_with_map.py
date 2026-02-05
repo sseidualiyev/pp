92a1c1a77e8c9da9148ef6855c84bbbb57dd98d3
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:03:04 +0500

    using lambda and map to convert

--- practice3/lambda/lambda_with_map.py
+++ practice3/lambda/lambda_with_map.py
@@ -6,3 +6,9 @@ nums = [1, 2, 3, 4, 5]
 
 squared = list(map(lambda x: x**2, nums))
 print(squared)
+
+
+celsius = [0, 10, 20, 30]
+
+fahrenheit = list(map(lambda c: c * 9/5 + 32, celsius))
+print(fahrenheit)
