eee9bb20249a67ebc9aa99651c74159c989fce2c
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:07:19 +0500

    sort a list of tuples in pair

--- practice3/lambda/lambda_with_sorted.py
+++ practice3/lambda/lambda_with_sorted.py
@@ -11,3 +11,9 @@ words = ["apple", "kiwi", "banana", "fig"]
 
 by_length = sorted(words, key=lambda w: len(w))
 print(by_length)
+
+
+pairs = [(1, 3), (4, 1), (2, 2)]
+
+sorted_pairs = sorted(pairs, key=lambda x: x[1])
+print(sorted_pairs)
