38b04529600a66ded19759c52d5ea229630deb61
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:19:40 +0500

    setting a default values in init parameters

--- practice3/classes/init_method.py
+++ practice3/classes/init_method.py
@@ -17,3 +17,14 @@ p1.age = 25
 
 print(p1.name)
 print(p1.age)
+
+class Person:
+  def __init__(self, name, age=18):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil")
+p2 = Person("Tobias", 25)
+
+print(p1.name, p1.age)
+print(p2.name, p2.age)
