e8de4cfb299da865f033dd9dced55d60f8efce12
sseidualiev <seidualiev01@gmail.com>
2026-02-05 21:06:40 +0500

    sort numbers in descending

--- practice3/lambda/lambda_with_sorted.py
+++ practice3/lambda/lambda_with_sorted.py
@@ -1,3 +1,8 @@
 students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
 sorted_students = sorted(students, key=lambda x: x[1])
 print(sorted_students)
+
+nums = [5, 2, 9, 1, 7]
+
+desc = sorted(nums, key=lambda x: -x)
+print(desc)
