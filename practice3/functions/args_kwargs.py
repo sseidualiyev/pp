4ea56b59b4fe6b4d425cefc86809ce4fffabca65
sseidualiev <seidualiev01@gmail.com>
2026-02-05 20:44:24 +0500

    unpacking lists with args

--- practice3/functions/args_kwargs.py
+++ practice3/functions/args_kwargs.py
@@ -65,3 +65,10 @@ def my_function(title, *args, **kwargs):
   print("Keyword arguments:", kwargs)
 
 my_function("User Info", "Emil", "Tobias", age = 25, city = "Oslo")
+
+def my_function(a, b, c):
+  return a + b + c
+
+numbers = [1, 2, 3]
+result = my_function(*numbers) # Same as: my_function(1, 2, 3)
+print(result)
