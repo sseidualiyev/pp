a1f25650abcbe5038d28887ea786b5a8a67b309d
sseidualiev <seidualiev01@gmail.com>
2026-02-18 20:47:34 +0500

    Calculate the area of a trapezoid

--- practice4/math.py
+++ practice4/math.py
@@ -2,3 +2,9 @@ import math
 degree = float(input("Input degree: "))
 radian = degree * (math.pi / 180)
 print("Output radian:", round(radian, 6))
+
+height = float(input("Height: "))
+base1 = float(input("Base, first value: "))
+base2 = float(input("Base, second value: "))
+area_trapezoid = 0.5 * (base1 + base2) * height
+print("Expected Output:", area_trapezoid)
