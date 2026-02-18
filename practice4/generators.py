c352223a042a83b2612ee93de8c4cb929ef09884
sseidualiev <seidualiev01@gmail.com>
2026-02-18 20:37:39 +0500

    Generator for numbers divisible by both 3 and 4 between 0 and n

--- practice4/generators.py
+++ practice4/generators.py
@@ -5,3 +5,8 @@ def squares_up_to_n(N):
 def even_numbers(n):
     for i in range(0, n + 1, 2):
         yield i
+
+def divisible_by_3_and_4(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
