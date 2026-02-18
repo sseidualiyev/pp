57b7f8a20c0f1da58858a59142c56227b90381a5
sseidualiev <seidualiev01@gmail.com>
2026-02-18 20:42:32 +0500

    Drop microseconds from datetime

--- practice4/date.py
+++ practice4/date.py
@@ -10,3 +10,7 @@ tomorrow = today + timedelta(days=1)
 print("Yesterday:", yesterday)
 print("Today:", today)
 print("Tomorrow:", tomorrow)
+
+from datetime import datetime
+now = datetime.now().replace(microsecond=0)
+print(now)
