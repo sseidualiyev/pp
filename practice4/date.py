7adc76466add2b5752c7a2c5633e5fc36418c6c3
sseidualiev <seidualiev01@gmail.com>
2026-02-18 20:42:05 +0500

    Print yesterday, today, tomorrow

--- practice4/date.py
+++ practice4/date.py
@@ -2,3 +2,11 @@ from datetime import datetime, timedelta
 current_date = datetime.now()
 new_date = current_date - timedelta(days=5)
 print(new_date)
+
+from datetime import datetime, timedelta
+today = datetime.now()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+print("Yesterday:", yesterday)
+print("Today:", today)
+print("Tomorrow:", tomorrow)
