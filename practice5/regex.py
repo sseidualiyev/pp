e920484e1709062769282cd8d750bf3d708777eb
sseidualiev <seidualiev01@gmail.com>
2026-02-27 22:36:44 +0500

    regex ex 2

--- practice5/regex.py
+++ practice5/regex.py
@@ -6,3 +6,10 @@ if re.fullmatch(r"ab*", s):
     print("Match")
 else:
     print("No match")
+
+# 2
+s = input("Task 2: ")
+if re.fullmatch(r"ab{2,3}", s):
+    print("Match")
+else:
+    print("No match")
