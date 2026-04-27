beb8fa6869e2ede45f881eccb83f0e6ba735e788
sseidualiev <seidualiev01@gmail.com>
2026-04-27 23:39:37 +0500

    add contacts function

--- TSIS/TSIS 1/phonebook.py
+++ TSIS/TSIS 1/phonebook.py
@@ -24,3 +24,40 @@ def get_contact_id(cur, name):
     return res[0] if res else None
 
 
+# =========================
+# ADD CONTACT
+# =========================
+
+def add_contact(conn):
+    cur = conn.cursor()
+
+    name = input("Name: ")
+    email = input("Email: ")
+    birthday = input("Birthday (YYYY-MM-DD): ")
+    group = input("Group: ")
+
+    gid = get_group_id(cur, group)
+
+    cur.execute("""
+        INSERT INTO contacts(name, email, birthday, group_id)
+        VALUES (%s, %s, %s, %s)
+        RETURNING id
+    """, (name, email, birthday, gid))
+
+    cid = cur.fetchone()[0]
+
+    while True:
+        phone = input("Phone (or empty to stop): ")
+        if not phone:
+            break
+        ptype = input("Type (home/work/mobile): ")
+
+        cur.execute("""
+            INSERT INTO phones(contact_id, phone, type)
+            VALUES (%s, %s, %s)
+        """, (cid, phone, ptype))
+
+    conn.commit()
+    print("✅ Contact added.")
+
+
