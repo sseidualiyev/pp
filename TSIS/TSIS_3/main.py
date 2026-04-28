b7fc4bb21277b1fff0719968accea30c3d0753f2
sseidualiev <seidualiev01@gmail.com>
2026-04-28 21:20:33 +0500

    load assets

--- TSIS/TSIS_3/main.py
+++ TSIS/TSIS_3/main.py
@@ -10,3 +10,10 @@ pygame.init()
 screen = pygame.display.set_mode((480, 800))
 font = pygame.font.SysFont("Verdana", 24)
 
+# assets
+assets = {
+    "player": pygame.image.load("assets/Player.png").convert_alpha(),
+    "enemy": pygame.image.load("assets/Enemy.png").convert_alpha(),
+    "coin": pygame.image.load("assets/coin.png").convert_alpha()
+}
+
