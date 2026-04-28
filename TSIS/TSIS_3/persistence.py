d38c3e8f52b3fe01de12384c3fdd05a5b1781356
sseidualiev <seidualiev01@gmail.com>
2026-04-28 21:08:17 +0500

    loading settings file

--- TSIS/TSIS_3/persistence.py
+++ TSIS/TSIS_3/persistence.py
@@ -5,3 +5,14 @@ SETTINGS_FILE = "settings.json"
 LEADERBOARD_FILE = "leaderboard.json"
 
 
+def load_settings():
+    if not os.path.exists(SETTINGS_FILE):
+        return {
+            "sound": True,
+            "car_color": "blue",
+            "difficulty": "normal"
+        }
+    with open(SETTINGS_FILE, "r") as f:
+        return json.load(f)
+
+
