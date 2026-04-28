66b3137b81b58987c47cf12054ab96f6dcf24280
sseidualiev <seidualiev01@gmail.com>
2026-04-28 21:10:46 +0500

    implement power ups

--- TSIS/TSIS_3/racer.py
+++ TSIS/TSIS_3/racer.py
@@ -8,3 +8,21 @@ SCREEN_HEIGHT = 800
 LANES = [120, 240, 360]
 
 
+# ---------------- POWER UPS ----------------
+class PowerUp:
+    def __init__(self, image):
+        self.image = image
+        self.type = random.choice(["nitro", "shield", "repair"])
+        self.spawn()
+
+    def spawn(self):
+        self.lane = random.choice(LANES)
+        self.rect = self.image.get_rect(center=(self.lane, -100))
+        self.speed = 5
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top > SCREEN_HEIGHT:
+            self.spawn()
+
+
